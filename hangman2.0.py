a055727245b165e864d86be13082201daeac3f8b
Russellup <59325336+Russellup@users.noreply.github.com>
2022-02-04 13:07:38 -0800

    Update hangman2.0.py

--- hangman2.0.py
+++ hangman2.0.py
@@ -3,15 +3,37 @@ import random
 class HangmanGame:
     
     def __init__(self):
-        self.wl = 'garrapolo, rice, williams, kittle '.split(', ') # wordlist
-        self.rw = None #randomword
-        self.hm = None # hangman
-        self.t  = 1 # turns
+        self.wordlist = 'garrapolo, rice, williams, kittle '.split(', ') 
+        self.randomword = None 
+        self.hangman = None 
+        self.turns  = 1 
 
     def start(self):
-        self.rw = random.choice(self.wl)
-        self.hm = len(self.rw)*['_']
+        self.randomword = random.choice(self.wordlist)
+        self.hangman = len(self.randomword)*['_']
 
     def is_running(self):
-        return True if '_' in self.hm else False
+        return True if '_' in self.hangman else False
+
+    def show_all(self):
+        if '_' in self.hangman:
+            print('\nHangmanGame: {}\nturns: {}\n'.format(' '.join(self.hangman), self.turns))
+        else:
+            print('\nHangmanGame Completed!: {}\nturns: {}\n'.format(''.join(self.hangman), self.turns))
+        print('---------------------------------------------------')
+
+    def compare(self, guess):
+        for i, char in enumerate(self.randomword):
+            if guess == char:
+                self.hangman[i] = char
+        self.turns +=1
+
+game = HangmanGame()
+game.start()
+
+while game.is_running():
+    game.show_all()
+    game.compare(input("Guess: "))
+   
+game.show_all()
     
